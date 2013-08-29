801a0c7438cb7f819825f80d430be5b69d671674
Robert Butts <robert.o.butts@gmail.com>
2013-08-28 21:33:34 -0500

    created quadtree package

--- quadtree.go
+++ quadtree.go
@@ -1,10 +1,7 @@
-package main
+package quadtree
 
 import (
-	"fmt"
 	"strconv"
-	"math/rand"
-	"time"
 )
 
 type Point struct {
@@ -34,8 +31,6 @@ func (b *BoundingBox) Intersects(other *BoundingBox) bool {
 		b.Center.Y+b.HalfDimension.Y > other.Center.Y-other.HalfDimension.Y &&
 		b.Center.Y-b.HalfDimension.Y < other.Center.Y+other.HalfDimension.Y
 }
-
-
 
 type Quadtree struct {
 	Boundary *BoundingBox
@@ -138,38 +133,4 @@ func NewQuadTree(b *BoundingBox, capacity int) *Quadtree {
 		Boundary: b,
 		Points: make([]Point, 0, capacity),
 	}
-}
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-func main() {
-	nodeCapacity := 4
-	qt := NewQuadTree(&BoundingBox {
-		Center: Point { 100.0, 100.0 },
-		HalfDimension: Point { 50.0, 50.0,},
-	}, nodeCapacity)
-
-	pointsToInsert := 10000000
-	start := time.Now()
-	for i := 0; i != pointsToInsert; i++ {
-		p := Point{rand.Float64() * 100.0 + 50.0, rand.Float64() * 100.0 + 50.0}
-		if !qt.Insert(p) {
-		}
-	}
-	end := time.Now()
-	elapsed := end.Sub(start)
-	fmt.Println("inserted " + strconv.Itoa(pointsToInsert) + " points in " + elapsed.String() + ".")
-/*
-	qr := &BoundingBox {
-		Center: Point { 75.0, 75.0 },
-		HalfDimension: Point { 5.0, 5.0,},
-	}
-	points := qt.QueryRange(qr)
-	fmt.Println("found " + strconv.Itoa(len(points)) + " points")
-*/
-//	for _, point := range points {
-//		fmt.Println(point.String())
-//	}
 }
