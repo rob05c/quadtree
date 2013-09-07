5b4d490bc6fa6432575b9b314c5635a2a79f2ca6
Rob <rob@dell42.(none)>
2013-09-06 21:36:59 -0600

    fixed benchmark test to conform to go test

---
+++ quadtree_test.go
@@ -0,0 +1,64 @@
+package quadtree
+
+import (
+	"time"
+	"runtime"
+	"math/rand"
+	"testing"
+)
+
+func benchInsert(b *testing.B) *Quadtree {
+	b.StopTimer()
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	rand.Seed(time.Now().UnixNano())
+
+	nodeCapacity := 4
+	qt := New(&BoundingBox{
+		Center:        Point{100.0, 100.0},
+		HalfDimension: Point{50.0, 50.0},
+	}, nodeCapacity)
+
+	pointsToInsert := b.N
+
+	threads := runtime.NumCPU()
+
+	tpoints := pointsToInsert / threads
+	done := make(chan bool)
+
+	insertPoint := func() {
+		for i := 0; i != tpoints; i++ {
+			p := &Point{rand.Float64()*100.0 + 50.0, rand.Float64()*100.0 + 50.0}
+			qt.Insert(p)
+		}
+		done <- true
+	}
+	b.StartTimer()
+	b.ResetTimer()
+	for i := 0; i != threads; i++ {
+		go insertPoint()
+	}
+	for i := 0; i != threads; i++ {
+		<- done
+	}
+	return qt
+}
+
+func BenchmarkInsert(b *testing.B) {
+	benchInsert(b)
+}
+
+func BenchmarkQuery(b *testing.B) {
+	b.StopTimer()
+	qt := benchInsert(b)
+	queries := b.N
+	box := &BoundingBox {
+		Center: Point { 100.0, 100.0 },
+		HalfDimension: Point { 5.0, 5.0,},
+	}
+	b.StartTimer()
+	b.ResetTimer()
+	for i := 0; i != queries; i++ {
+		box.Center = Point{rand.Float64()*100.0 + 50.0, rand.Float64()*100.0 + 50.0}
+		qt.Query(box)
+	}
+}
